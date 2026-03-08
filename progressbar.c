48c76f9432ca61466571af951fb3ff6bb5e9c37c
Kühl <felipekuhl321@gmail.com>
2026-03-08 17:48:19 -0300

    Day 3: fix progress bar

--- progressbar.c
+++ progressbar.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define PROGRESS_MAX 10
-
+#define PROGRESS 100
+#define BAR_WIDTH 16
 int main(void)
 {
-    for(int i=0; i<=PROGRESS_MAX; i++)
+    for(int i=0; i<=PROGRESS; i+=1)
     {
-        usleep(500000);
+        usleep(32000);
         printf("\r");
-        printf("[");
-        int p = 0;
-        while(p < PROGRESS_MAX)
+        printf("Progress: [");
+
+        for(int p=0; p<BAR_WIDTH; p++)
         {
-            p++;
-            for(int i=0; i<PROGRESS_MAX; i++)
+            if(p < ((float)i/PROGRESS*BAR_WIDTH))
             {
-                if(i < p)
-                {
-                    printf("#");
-                }
-                else
-                {
-                    printf("-");
-                }
+                printf("#");
+            }
+            else
+            {
+                printf("-");
             }
         }
 
         printf("]");
+        printf(" %i%%", i);
         fflush(stdout);
     }
-
     return 0;
 }
