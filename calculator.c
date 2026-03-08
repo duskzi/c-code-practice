c9b349e60f93683dff86ce0cc05b6b94dc83bbd5
Kühl <felipekuhl321@gmail.com>
2026-03-08 12:13:58 -0300

    3 Day: Calc bug fixes and improvements

--- calculator.c
+++ calculator.c
@@ -12,34 +12,36 @@ Thanks for reading, Dusk.
 
 #include <stdio.h>
 
-
-float add(float a, float b)
-{
+float add(float a, float b){
     return a + b;
 }
 
-float subtract(float a, float b)
-{
+float subtract(float a, float b){
     return a - b;
 }
 
-float multiply(float a, float b)
-{
+float multiply(float a, float b){
     return a * b;
 }
 
-float divide(float a, float b)
-{
+float divide(float a, float b){   
     return a / b;
 }
 
+int scan_input(float *input){
+    if(scanf("%f", input) != 1)
+    {
+        printf("Invalid input\n");
+        return 1;
+    }
+    // Safety purposes
+    return 0;
+}
 
-int main(void)
-{
-    int cont = 1;
-
-    while(cont) {
-        printf("What operation would you choose?\n\n");
+int main(void){
+    // True because i'm returning with 0
+    while(1) {
+        printf("What operation would you choose?\n");
 
         printf("[1] Add\n");
         printf("[2] Subtract\n");
@@ -48,23 +50,33 @@ int main(void)
         printf("[0] Exit\n");
 
         int option;
-        scanf("%i", &option);
 
-        if(option == 0.0)
-        {
-            cont = 0;
+        // Don't know why BUT it's a buffer
+        // issue that happens
+        if (scanf("%i", &option) != 1) {
+            printf("Invalid option\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        if(option == 0){
             return 0;
         }
 
-        float a, b;
-        printf("Type your first number\n");
-        scanf("%f", &a);
-        printf("Type your second number\n");
-        scanf("%f", &b);
+        if(option < 1 || option > 4){
+            printf("Invalid option\n");
+            return 0;
+        }
 
         float result;
+        float a, b;
+        printf("\nType your first number: ");
+        if(scan_input(&a)) continue;
+        printf("Type your second number: ");
+        if(scan_input(&b)) continue;
+        printf("\n");
 
-        switch(option) {
+        switch(option){
             case 1:
                 result = add(a, b);
                 break;
@@ -72,14 +84,20 @@ int main(void)
                 result = subtract(a, b);
                 break;
             case 3:
+                if(b == 0.0){
+                    printf("-> Can't divide by 0\n");
+                    continue;
+                }   
                 result = divide(a, b);
                 break;
             case 4:
                 result = multiply(a, b);
                 break;
+            default:
+                printf("Something went wrong...\n");
+                return 0;
         }
         printf("-> %f\n", result);
     }
-
     return 0;
 }
