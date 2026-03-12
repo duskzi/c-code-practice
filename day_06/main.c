fd35abe7fd3b730dff331bd24546ed34b0af3d09
Kühl <felipekuhl321@gmail.com>
2026-03-11 22:17:42 -0300

    Day 6: Finish word frequency counter

--- day_06/main.c
+++ day_06/main.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-// Exercise: Char frequency
-// Let's get it
 
-// Create array for char data
-// Using {} here to fill array with 0
-
-const int STR_LENGHT = 256;
+#define CHAR_LENGHT 256
 
 int main(void)
 {
-    int char_data[256] = {0};
-
-    char str[STR_LENGHT];
+    char char_data[CHAR_LENGHT] = {0};
 
     FILE *f = fopen("./file.txt", "r");
     if(f == NULL)
@@ -22,16 +15,27 @@ int main(void)
 
     int c;
     int i = 0;
-    while((c = fgetc(f)) != EOF && i < 255)
+    // Read document char by char
+    while((c = fgetc(f)) != EOF)
     {
-        str[i] = c;
+        // Store frequency of each one
+        char_data[c] += 1;
         i++;
     }
 
-    printf("%s", str);
+    // Listen, brain
+    // J      -> char
+    // data   -> Frequency
+    // J + %d -> Ascci value
+    int data;
+    for(int j=0; j<CHAR_LENGHT; j++)
+    {  
+        data = char_data[j];
+        if(!(data == 0 || j=='\n' || j==' '))
+        {
+            printf("%c: %d | ascci value: %d\n", j, data, j);
+        }
+    }
+
+    fclose(f);
 }
-
-// Read document char by char
-
-// Store frequency of each one
-
